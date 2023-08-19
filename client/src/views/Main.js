415b912c2c55cca18f519be88b8226b3efb0b7fc
Babak Jahedmanesh <babak.jahedmanesh@thenavpeople.co.uk>
2023-08-19 01:26:28 +0100

    main forms & samples

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -49,7 +49,36 @@ function Main() {
                         </h2>
                         <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
                             <div class="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <table class="table table-striped-columns">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">First</th>
+                                            <th scope="col">Last</th>
+                                            <th scope="col">Handle</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry the Bird</td>
+                                            <td>@twitter</td>
+                                            <td>@dude</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -61,7 +90,41 @@ function Main() {
                         </h2>
                         <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
                             <div class="accordion-body">
-                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1">@</span>
+                                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                                    <span class="input-group-text" id="basic-addon2">@example.com</span>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="basic-url" class="form-label">Your vanity URL</label>
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
+                                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4" />
+                                    </div>
+                                    <div class="form-text" id="basic-addon4">Example help text goes outside the input group.</div>
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">$</span>
+                                    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" />
+                                    <span class="input-group-text">.00</span>
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" />
+                                    <span class="input-group-text">@</span>
+                                    <input type="text" class="form-control" placeholder="Server" aria-label="Server" />
+                                </div>
+
+                                <div class="input-group">
+                                    <span class="input-group-text">With textarea</span>
+                                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -73,7 +136,51 @@ function Main() {
                         </h2>
                         <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
                             <div class="accordion-body">
-                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <div class="input-group mb-3">
+                                    <div class="input-group-text">
+                                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" />
+                                    </div>
+                                    <input type="text" class="form-control" aria-label="Text input with checkbox" />
+                                </div>
+
+                                <div class="input-group">
+                                    <div class="input-group-text">
+                                        <input class="form-check-input mt-0" type="radio" value="" aria-label="Radio button for following text input" />
+                                    </div>
+                                    <input type="text" class="form-control" aria-label="Text input with radio button" />
+                                </div>
+                                <div class="input-group mb-3">
+                                    <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
+                                    <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" />
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <button class="btn btn-outline-secondary" type="button">Button</button>
+                                    <button class="btn btn-outline-secondary" type="button">Button</button>
+                                    <input type="text" class="form-control" placeholder="" aria-label="Example text with two button addons" />
+                                </div>
+
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username with two button addons" />
+                                    <button class="btn btn-outline-secondary" type="button">Button</button>
+                                    <button class="btn btn-outline-secondary" type="button">Button</button>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        <li><hr class="dropdown-divider" /></li>
+                                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                                    </ul>
+                                    <input type="text" class="form-control" aria-label="Text input with dropdown button" />
+                                </div>
                             </div>
                         </div>
                     </div>
